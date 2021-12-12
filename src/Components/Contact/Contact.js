4a2048db270517515dd766b528cabac4f4413a49
Teresa Franxman <teresafranxman97@gmail.com>
2021-12-11 22:34:22 -0600

    Created contact form and state

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { send, init } from 'emailjs-com';
 
 function Contact() {
+    const [ toSend, setToSet ] = useState({
+        user_name: '',
+        user_email: '',
+        message: ''
+    });
+
     return (
         <div>
-            Contact
+           <div className="form-container"> 
+                <form>
+                    <input 
+                        type="text"
+                        name="user_name"
+                        placeholder="NAME"
+                    />
+                    <input
+                        type="text"
+                        name="user_email"
+                        placeholder="EMAIL"
+                    />
+                    <input
+                        type="text"
+                        name="message"
+                        placeholder="MESSAGE..."
+                    />
+                </form>
+            </div> 
         </div>
     );
 }
