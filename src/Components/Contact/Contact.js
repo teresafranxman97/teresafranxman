a89e95c3fce71d319081191ecdd007867d241e20
Teresa Franxman <teresafranxman97@gmail.com>
2021-12-23 00:56:59 -0600

    Form now clears after submission, alert is displayed on success

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,67 +1,82 @@
 import React, { useState } from "react";
-import { send } from 'emailjs-com';
+import { send } from "emailjs-com";
 
 function Contact() {
-    const [ toSend, setToSend ] = useState({
-        user_name: '',
-        user_email: '',
-        message: ''
-    });
+	const [toSend, setToSend] = useState({
+		user_name: "",
+		user_email: "",
+		message: "",
+	});
 
-    const onSubmit = (e) => {
-        e.preventDefault();
-        send(
-            'service_1gaftse',
-            'contact_form',
-            toSend,
-            'user_08idYLJZO7AtHuPUQ55EA'
-        )
-        .then((res) => {
-            console.log("Success!", res.status, res.text)
-        })
-        .catch((err) => {
-            console.log("Failed...", err)
-        })
-    }
+	const handleChange = (e) => {
+		setToSend({
+			...toSend,
+			[e.target.name]: e.target.value,
+		});
+	};
 
-    const handleChange = (e) => {
-        setToSend({
-            ...toSend, [e.target.name]: e.target.value
-        })
-    }
+	const resetForm = () => {
+		setToSend({
+			user_name: "",
+			user_email: "",
+			message: "",
+		})
+	}
 
-    return (
-        <div>
-           <div className="form-container"> 
-                <form onSubmit={onSubmit}>
-                    <input 
-                        type="text"
-                        name="user_name"
-                        placeholder="NAME"
-                        value={toSend.user_name}
-                        onChange={handleChange}
-                    />
-                    <input
-                        type="text"
-                        name="user_email"
-                        placeholder="EMAIL"
-                        value={toSend.user_email}
-                        onChange={handleChange}
-                    />
-                    <input
-                        type="text"
+	const onSubmit = (e) => {
+		e.preventDefault();
+		send(
+			"service_1gaftse",
+			"contact_form",
+			toSend,
+			"user_08idYLJZO7AtHuPUQ55EA"
+		)
+			.then((res) => {
+				console.log("Success!", res.status, res.text, res);
+				alert("Your message was sent successfully!")
+			})
+			.catch((err) => {
+				console.log("Failed...", err);
+			});
+
+		resetForm()
+	};
+
+	return (
+		<div>
+			<div className="form-container">
+				<form onSubmit={onSubmit}>
+                    <label>Name</label>
+					<input
+						type="text"
+						name="user_name"
+						placeholder="NAME"
+						value={toSend.user_name}
+						onChange={handleChange}
+					/>
+                    <label>Email</label>
+					<input
+						type="text"
+						name="user_email"
+						placeholder="EMAIL"
+						value={toSend.user_email}
+						onChange={handleChange}
+					/>
+                    <label>Message</label>
+					<textarea
+                        required
                         name="message"
-                        placeholder="MESSAGE..."
                         value={toSend.message}
                         onChange={handleChange}
-                    />
-                    <div className="button-container">
-                        <button type="submit">Submit</button>
-                    </div>
-                </form>
-            </div> 
-        </div>
-    );
+                    ></textarea>
+
+					<div className="button-container">
+						<button type="submit">Submit</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
 }
 
 export default Contact;
