de55388790afd5b38bafe91234cdc2f36a4a324b
Teresa <teresafranxman97@gmail.com>
2022-01-26 21:30:57 -0600

    Tools.js styles now responsive

--- src/Components/Tools/Tools.js
+++ src/Components/Tools/Tools.js
@@ -1,23 +1,23 @@
 import React, { useState } from "react";
 import data from "./tools-data";
 import { Icon } from "@iconify/react";
-import StyledSkills from "../../Styles/Skills/skills-styles";
+import StyledTools from "../../Styles/Tools/toolsStyles";
 
 const Tools = () => {
 	const [skills] = useState(data);
 
 	return (
-		<StyledSkills>
+		<StyledTools>
             <h1>Tools I use to plan, design, and build quality applications. </h1>
-			<div className="content">
+			<div className="content-grid">
 				{skills.map((skill) => (
 					<div className="skills-container" key={skill.id}>
-						<Icon icon={skill.icon} alt="logo" width={80} className="icon" />
+						<Icon icon={skill.icon} alt="logo" width={40} className="icon" />
 						<h3>{skill.title}</h3>
 					</div>
 				))}
 			</div>
-		</StyledSkills>
+		</StyledTools>
 	);
 };
 
---
+++ src/Styles/Tools/toolsStyles.js
@@ -0,0 +1,56 @@
+import styled from "styled-components";
+
+const StyledTools = styled.div`   
+    padding: 7rem;
+
+    .content-grid {
+        display: grid;
+        gap: 1rem;
+        grid-template-columns: repeat(5, 1fr);
+    }
+    
+    .skills-container {
+		display: flex;
+		flex-flow: column wrap;
+		align-items: center;
+        text-align: center;
+        justify-content: center;
+        background: #F0EFF0;
+	}
+
+	h1 {
+        width: 50vw;
+        font-weight: normal;
+	}
+
+    .icon {
+        color: black;
+        border-radius: 5px 10px;
+        padding: .5em;
+    }
+
+    h3 {
+        font-size: .85rem;
+    }
+
+    @media (max-width: 780px) {
+        padding: 3rem;
+
+        .content-grid {
+            grid-template-columns: repeat(3, 1fr);
+        }
+
+        h1 {
+            width: auto;
+            font-weight: normal;
+        }
+    }
+
+    @media (max-width: 480px) {
+        padding: 2rem;
+    }
+
+	
+`;
+
+export default StyledTools;
