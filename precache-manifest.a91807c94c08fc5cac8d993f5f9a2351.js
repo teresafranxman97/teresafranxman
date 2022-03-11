a08c66247e778c7b6bea99509740cad64c6a6edb
Teresa Franxman <teresafranxman97@gmail.com>
2022-03-11 08:42:14 -0600

    Updates

---
+++ precache-manifest.a91807c94c08fc5cac8d993f5f9a2351.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1540398a3538392b813232cdea926a04",
+    "url": "/teresafranxman/index.html"
+  },
+  {
+    "revision": "2c9824455958b9536362",
+    "url": "/teresafranxman/static/css/main.035a2bb8.chunk.css"
+  },
+  {
+    "revision": "e781ab8796432f7d0dfd",
+    "url": "/teresafranxman/static/js/2.e6593c3b.chunk.js"
+  },
+  {
+    "revision": "2e7a0b14f6d1081167a5a052c8545f6d",
+    "url": "/teresafranxman/static/js/2.e6593c3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1017b633dcc517d6f59",
+    "url": "/teresafranxman/static/js/3.4605fe91.chunk.js"
+  },
+  {
+    "revision": "2c9824455958b9536362",
+    "url": "/teresafranxman/static/js/main.00e94574.chunk.js"
+  },
+  {
+    "revision": "fd3a3be1fc06d4acf6ee",
+    "url": "/teresafranxman/static/js/runtime-main.237718b2.js"
+  },
+  {
+    "revision": "eefb0444778c50607765783dea76e7be",
+    "url": "/teresafranxman/static/media/googleclone.eefb0444.png"
+  },
+  {
+    "revision": "f96013a3632846589a1ce0cdb5aef582",
+    "url": "/teresafranxman/static/media/underdogdevs.f96013a3.png"
+  }
+]);
