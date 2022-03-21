c2b1f95e3792a163b00f776f1201143383451579
Teresa Franxman <teresafranxman97@gmail.com>
2022-03-21 15:27:03 -0500

    [Bug fix]

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter} from "react-router-dom";
+import { BrowserRouter as Router} from "react-router-dom";
 import './index.css';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <Router>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </BrowserRouter>,
+  </Router>,
   document.getElementById('root')
 );
 
